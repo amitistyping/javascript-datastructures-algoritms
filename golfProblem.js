f21a8a508726e5fd549840fee7647249376bf3e4
Amitoj Singh <amitoj.dev@gmail.com>
2023-02-01 17:06:02 -0400

    golf problem

--- golfProblem.js
+++ golfProblem.js
@@ -1,3 +1,31 @@
 /**
  * https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/golf-code
  */
+
+//Solution 1
+(async () => {
+	const names = ['Hole-in-one!', 'Eagle', 'Birdie', 'Par', 'Bogey', 'Double Bogey', 'Go Home!'];
+
+	function golfScore(par, strokes) {
+		// Only change code below this line
+		switch (true) {
+			case strokes == 1:
+				return 'Hole-in-one!';
+			case strokes <= par - 2:
+				return 'Eagle';
+			case strokes == par - 1:
+				return 'Birdie';
+			case strokes == par:
+				return 'Par';
+			case strokes == par + 1:
+				return 'Bogey';
+			case strokes == par + 2:
+				return 'Double Bogey';
+			case strokes >= par + 3:
+				return 'Go Home!';
+			default:
+				return;
+		}
+		// Only change code above this line
+	}
+})();
